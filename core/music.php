1bba592baef07aa140d8b3b32571ecf1a0a6bc03
Pawliner <93891278+Pawliner@users.noreply.github.com>
2020-05-16 15:33:48 +0000

    Remove unused import

---
+++ core/music.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ *
+ * 音乐搜索器 - 函数声明
+ *
+ * @author  MaiCong <i@maicong.me>
+ * @link    https://github.com/maicong/music
+ * @since   1.6.2
+ *
+ */
+
+// 非我族类
+if (!defined('MC_CORE')) {
+    header("Location: /");
+    exit();
+}
+
+// 显示 PHP 错误报告
