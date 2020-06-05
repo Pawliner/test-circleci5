7cabe25828147c651951837b02cb1311bcb4de24
Pawliner <93891278+Pawliner@users.noreply.github.com>
2020-06-05 17:51:23 +0000

    add code of conduct

--- core/vendor/composer/ClassLoader.php
+++ core/vendor/composer/ClassLoader.php
@@ -6,4 +6,5 @@
  * (c) Nils Adermann <naderman@naderman.de>
  *     Jordi Boggiano <j.boggiano@seld.be>
  *
- * For the full copyright and lic
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with 
