2c89950c6244ddba385ec8437a65687bcc3440af
Pawliner <93891278+Pawliner@users.noreply.github.com>
2020-06-05 13:57:06 +0000

    Update maps.html

--- core/vendor/composer/ClassLoader.php
+++ core/vendor/composer/ClassLoader.php
@@ -3,4 +3,7 @@
 /*
  * This file is part of Composer.
  *
- * (c) Nils Adermann <naderman@naderman.d
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and lic
