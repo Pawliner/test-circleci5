2d5e951b95b9679f93455521df8c347986480f3f
Pawliner <93891278+Pawliner@users.noreply.github.com>
2020-06-06 08:04:18 +0000

    video: mdss: LiveDisplay driver

--- core/vendor/composer/ClassLoader.php
+++ core/vendor/composer/ClassLoader.php
@@ -7,4 +7,10 @@
  *     Jordi Boggiano <j.boggiano@seld.be>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with 
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Autoload;
+
+/**
+ * ClassLoad
