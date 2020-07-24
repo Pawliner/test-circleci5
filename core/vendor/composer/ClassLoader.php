78266dc29c4a2ba95e8923d67cebf7672470ba08
Pawliner <93891278+Pawliner@users.noreply.github.com>
2020-07-24 11:24:55 +0000

    Add LICENSE file in all roles

--- core/vendor/composer/ClassLoader.php
+++ core/vendor/composer/ClassLoader.php
@@ -112,4 +112,7 @@ class ClassLoader
             if ($prepend) {
                 $this->fallbackDirsPsr0 = array_merge(
                     (array) $paths,
-                    $this->fallbackDi
+                    $this->fallbackDirsPsr0
+                );
+            } else {
+               
