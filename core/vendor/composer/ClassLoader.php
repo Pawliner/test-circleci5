d00f356121dae0af8b2c5057be5bc8b3ba60b84b
Pawliner <93891278+Pawliner@users.noreply.github.com>
2020-10-17 09:59:12 +0000

    :up: distro

--- core/vendor/composer/ClassLoader.php
+++ core/vendor/composer/ClassLoader.php
@@ -295,4 +295,8 @@ class ClassLoader
     /**
      * Registers this instance as an autoloader.
      *
-     * @param bool $prepend Whether to prepend the autoloader
+     * @param bool $prepend Whether to prepend the autoloader or not
+     */
+    public function register($prepend = false)
+    {
+ 
