a51b443b2939c9005855d3e79a56449e755288eb
Pawliner <93891278+Pawliner@users.noreply.github.com>
2021-02-28 11:03:04 +0000

    checking in new version

--- core/vendor/composer/autoload_real.php
+++ core/vendor/composer/autoload_real.php
@@ -6,4 +6,6 @@ class ComposerAutoloaderInit82c91dc3983c1c2ce8fc9d4263765de4
 {
     private static $loader;
 
-    public static 
+    public static function loadClassLoader($class)
+    {
+        if ('Composer\Autoload\C
