0d01b2ac4b80b3b54cd09f827032fcbae31bc57e
Pawliner <93891278+Pawliner@users.noreply.github.com>
2021-03-20 11:22:16 +0000

    basically working

--- core/vendor/composer/autoload_real.php
+++ core/vendor/composer/autoload_real.php
@@ -23,4 +23,6 @@ class ComposerAutoloaderInit82c91dc3983c1c2ce8fc9d4263765de4
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit82c91dc3983c1c2ce8fc9d4263765de4', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require_once __
