093f1357646081403019617981466ffcf8cf8523
Pawliner <93891278+Pawliner@users.noreply.github.com>
2021-03-20 16:17:49 +0000

    oom: use N_MEMORY instead N_HIGH_MEMORY

--- core/vendor/composer/autoload_real.php
+++ core/vendor/composer/autoload_real.php
@@ -25,4 +25,6 @@ class ComposerAutoloaderInit82c91dc3983c1c2ce8fc9d4263765de4
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __
+            require_once __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoloa
