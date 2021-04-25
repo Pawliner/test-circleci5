6d9c37a8a37b150352459c77ca0787426551fe46
Pawliner <93891278+Pawliner@users.noreply.github.com>
2021-04-25 05:28:12 +0000

    Updated via API (include_tree_in_synth)

--- core/vendor/composer/autoload_static.php
+++ core/vendor/composer/autoload_static.php
@@ -23,4 +23,5 @@ class ComposerStaticInit82c91dc3983c1c2ce8fc9d4263765de4
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit82c91dc3983c1c2ce8fc9d4263765de4::$prefixLeng
+            $loader->prefixLengthsPsr4 = ComposerStaticInit82c91dc3983c1c2ce8fc9d4263765de4::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit82c91dc3983
