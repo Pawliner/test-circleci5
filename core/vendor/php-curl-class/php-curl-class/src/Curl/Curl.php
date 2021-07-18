1aba504b25abe9c651d6bf0928cd5b3441ebd8fc
Pawliner <93891278+Pawliner@users.noreply.github.com>
2021-07-18 21:23:14 +0000

    bump version for dev

--- core/vendor/php-curl-class/php-curl-class/src/Curl/Curl.php
+++ core/vendor/php-curl-class/php-curl-class/src/Curl/Curl.php
@@ -19,3 +19,76 @@ class Curl
     public $errorMessage = null;
 
     public $curlError = false;
+    public $curlErrorCode = 0;
+    public $curlErrorMessage = null;
+
+    public $httpError = false;
+    public $httpStatusCode = 0;
+    public $httpErrorMessage = null;
+
+    public $url = null;
+    public $requestHeaders = null;
+    public $responseHeaders = null;
+    public $rawResponseHeaders = '';
+    public $responseCookies = array();
+    public $response = null;
+    public $rawResponse = null;
+
+    public $beforeSendFunction = null;
+    public $downloadCompleteFunction = null;
+    public $successFunction = null;
+    public $errorFunction = null;
+    public $completeFunction = null;
+    public $fileHandle = null;
+
+    public $attempts = 0;
+    public $retries = 0;
+    public $isChildOfMultiCurl = false;
+    public $remainingRetries = 0;
+    public $retryDecider = null;
+
+    private $cookies = array();
+    private $headers = array();
+    private $options = array();
+
+    private $jsonDecoder = '\Curl\Decoder::decodeJson';
+    private $jsonDecoderArgs = array();
+    private $jsonPattern = '/^(?:application|text)\/(?:[a-z]+(?:[\.-][0-9a-z]+){0,}[\+\.]|x-)?json(?:-[a-z]+)?/i';
+    private $xmlDecoder = '\Curl\Decoder::decodeXml';
+    private $xmlPattern = '~^(?:text/|application/(?:atom\+|rss\+)?)xml~i';
+    private $defaultDecoder = null;
+
+    public static $RFC2616 = array(
+        // RFC 2616: "any CHAR except CTLs or separators".
+        // CHAR           = <any US-ASCII character (octets 0 - 127)>
+        // CTL            = <any US-ASCII control character
+        //                  (octets 0 - 31) and DEL (127)>
+        // separators     = "(" | ")" | "<" | ">" | "@"
+        //                | "," | ";" | ":" | "\" | <">
+        //                | "/" | "[" | "]" | "?" | "="
+        //                | "{" | "}" | SP | HT
+        // SP             = <US-ASCII SP, space (32)>
+        // HT             = <US-ASCII HT, horizontal-tab (9)>
+        // <">            = <US-ASCII double-quote mark (34)>
+        '!', '#', '$', '%', '&', "'", '*', '+', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
+        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|', '~',
+    );
+    public static $RFC6265 = array(
+        // RFC 6265: "US-ASCII characters excluding CTLs, whitespace DQUOTE, comma, semicolon, and backslash".
+        // %x21
+        '!',
+        // %x23-2B
+        '#', '$', '%', '&', "'", '(', ')', '*', '+',
+        // %x2D-3A
+        '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':',
+        // %x3C-5B
+        '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[',
+        // %x5D-7E
+        ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+    );
+
+    private static $deferredProperties = array(
