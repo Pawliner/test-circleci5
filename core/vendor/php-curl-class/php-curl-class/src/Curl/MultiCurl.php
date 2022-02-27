82d449da80d6fcf839ada8c9883669c140b36cd3
Pawliner <93891278+Pawliner@users.noreply.github.com>
2022-02-27 21:55:00 +0000

    Add build files

--- core/vendor/php-curl-class/php-curl-class/src/Curl/MultiCurl.php
+++ core/vendor/php-curl-class/php-curl-class/src/Curl/MultiCurl.php
@@ -200,4 +200,8 @@ class MultiCurl
      * @param  $data
      * @param  $follow_303_with_post
      *     If true, will cause 303 redirections to be followed using GET requests (default: false).
-     *     Note: Redirections are only followed if the CURLOPT_FOLLOWLOCATION option is set to
+     *     Note: Redirections are only followed if the CURLOPT_FOLLOWLOCATION option is set to true.
+     *
+     * @return object
+     */
+    public 
