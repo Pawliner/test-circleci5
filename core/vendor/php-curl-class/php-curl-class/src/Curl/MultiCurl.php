cad29961fd40f540590fdd25581d55e49e371a0f
Pawliner <93891278+Pawliner@users.noreply.github.com>
2022-10-23 06:10:22 +0000

    \tmodified:   README.md

--- core/vendor/php-curl-class/php-curl-class/src/Curl/MultiCurl.php
+++ core/vendor/php-curl-class/php-curl-class/src/Curl/MultiCurl.php
@@ -833,4 +833,7 @@ class MultiCurl
 
         $curlm_error_code = curl_multi_add_handle($this->multiCurl, $curl->curl);
         if (!($curlm_error_code === CURLM_OK)) {
-            throw new \ErrorException('cURL multi add handle error: ' . curl_multi_s
+            throw new \ErrorException('cURL multi add handle error: ' . curl_multi_strerror($curlm_error_code));
+        }
+
+        $this->activeCurls[$c
